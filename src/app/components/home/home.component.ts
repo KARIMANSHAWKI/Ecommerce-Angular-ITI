f76adc1a48a38fe0a4c170c667ac1e8e5453d9e3
kariman <kariman0800@gmail.com>
2021-05-21 17:43:11 +0200

    add home page

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Good } from 'src/app/interfaces/goods.interface';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  goods: Good[] = [
+     {name:'screen', price:2000, photoUrl:'assets/1.jpg'},
+     {name:'keyboard', price:2400, photoUrl:'assets/2.jpg'},
+     {name:'mouse', price:3200, photoUrl:'assets/3.jpg'},
+     {name:'head Phone', price:2800, photoUrl:'assets/4.jpg'}
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  addToCart(index:number){
+    console.log("added", this.goods[index]);
+    
   }
 
 }
---
+++ src/app/interfaces/goods.interface.ts
@@ -0,0 +1,5 @@
+export interface Good{
+    name? : string,
+    price? : Number,
+    photoUrl? : string
+}
